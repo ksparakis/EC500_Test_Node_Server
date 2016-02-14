283d1133c457f725a727506472dc71b2046790f2
jkonz <konz.jeremy@gmail.com>
2016-02-13 22:03:37 -0500

    Project completed
    Updated with completed project

--- jkonz/app.js
+++ jkonz/app.js
@@ -1,4 +1,7 @@
 ///////////////////// Node Password Server Begin /////////////////////
+var correctUsername = 'user';
+var correctPassword = 'qwerty123';
+
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
@@ -16,18 +19,36 @@ app.listen(nodePort, function () {
   console.log('node.js password application listening on port '+nodePort);
 });
 
+app.use(function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+next();
+});
+
 app.post('/login',function(req, res)
 {
 	var user = req.body.user;
 	var pw = req.body.pw;
+    var response;
 
 	console.log('Login request received from ' + req.url);
 	console.log('Username: ' + user );
 	console.log('Password: ' + pw);
 	res.setHeader('Content-Type', 'text/plain')
-	res.write('you posted:\n')
-	res.write(JSON.stringify(req.body.user, null, 2))
-	res.end('success')
+
+    if (user==correctUsername && pw ==correctPassword)
+    {
+        console.log('Login successful')
+        response = JSON.stringify({ status  : 200, success : 'Login successful.' });
+    }
+    else
+    {
+        console.log('Login failed')
+        response = JSON.stringify({ status: 99, error : 'Username or login credentials incorrect.' });
+    }
+
+    console.log('Responded with ' + response)
+    res.end(response);
 });
 ///////////////////// Node Password Server End ///////////////////////
 
---
+++ jkonz/readme.txt
@@ -0,0 +1,4 @@
+node.js project submitted by Jeremy Konz U09617043 on Feb 13 2016
+
+This project uses a single node instance to host a webpage (located at localhost:8081) to communicate to a server (localhost:8082) and emulates a simple user login. The webpage javascript sends the username and password in JSON format to the server. The server then compares the username and password and sends a response of success or failure. The expected username and password is user:qwerty123
+
